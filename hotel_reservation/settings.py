c24d9606791d3407210f4357f112fb19dd04db96
jshuva <jayanta.shuva@gmail.com>
2026-03-25 19:47:09 -0300

    Add Swagger UI documentation

--- hotel_reservation/settings.py
+++ hotel_reservation/settings.py
@@ -38,8 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular',
     'api',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
