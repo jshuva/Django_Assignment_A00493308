fc8f9c4d46e076efeb00fec81b22fd62abad4b92
jshuva <jayanta.shuva@gmail.com>
2026-03-25 21:25:02 -0300

    Update gitignore and track pre-populated SQLite database

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Hotel, Reservation, Guest
 
-# Register your models here.
+class GuestInline(admin.TabularInline):
+    model = Guest
+    extra = 1
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('confirmation_number', 'hotel_name', 'checkin', 'checkout')
+    search_fields = ('confirmation_number', 'hotel_name')
+    inlines = [GuestInline]
+
+@admin.register(Hotel)
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Guest)
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ('guest_name', 'gender', 'reservation')
+    search_fields = ('guest_name',)
